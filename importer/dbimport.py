5677b8fef1b5ba879695dff30039e6db83b964d8
John O'Hara <johara@gmail.com>
2012-10-26 09:24:28 -0400

    Moved the DBImport from the scraper/ folder to the new importer/ folder to keep everything organized.

---
+++ importer/dbimport.py
@@ -0,0 +1,42 @@
+import sys
+import fileinput
+import MySQLdb
+import importer
+
+db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="root", db="wdis")
+cur = db.cursor()
+
+predictionInsert = "INSERT into prediction (ida, idp, week, position, rankOrder) values ('%s','%s','%s','%s','%s')"
+
+if len(sys.argv) < 2:
+    print "Usage:  python dbimport.py <file-to-import> <file2> <file3>"
+    sys.exit(0)
+
+
+for fname in sys.argv[1:]:
+    lines = importer.ingestFile(fname)
+    
+    # The first line of the file contains the analystID, Name, and Station
+    analystid = lines[0].split(",")[0]
+    analystName = lines[0].split(",")[1]
+    analystStation = lines[0].split(",")[2]
+    
+    
+    for week in range(1,17):
+        for position in ("QB","RB","WR","TE"):
+            players =  importer.find(lines, str(week), position)
+            rank = 1
+            for player in players:
+                playerName = player.split(',')[0].strip()
+                playerTeam = player.split(',')[1].strip()
+                playerid = importer.lookupPlayer(cur, playerName, playerTeam, position)
+                cur.execute(predictionInsert % (analystid, playerid, str(week), position, str(rank)))
+                rank = rank + 1
+                cur.execute("commit")
+             
+        
+
+
+
+
+    
